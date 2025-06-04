a44a9f0bd7ab654dd3f622d59c24e232029c1239
Taha Rauf <rauftaha757@gmail.com>
2025-06-04 05:00:55 +0500

    Add: New car_ad_posting_route.js

---
+++ car_ad_posting_route.js
@@ -0,0 +1,92 @@
+const express = require("express");
+const CarAd_router = express.Router();
+const carAd_model = require("./carad_model");
+const { error } = require("console");
+const { find } = require("./carAD_model");
+const { Query } = require("mongoose");
+
+CarAd_router.post("/api/publishAd", async (req, res) => {
+  const {
+    milage,
+    imageurl,
+    transmition,
+    model,
+    registration,
+    carname,
+    company,
+    enginecapacity,
+    city,
+    color,
+    assembly,
+    contact,
+    createdAt,
+    price,
+  } = req.body;
+  try {
+    let carAd = new carAd_model({
+      milage,
+      imageurl,
+      transmition,
+      model,
+      registration,
+      carname,
+      company,
+      enginecapacity,
+      city,
+      color,
+      assembly,
+      contact,
+      price,
+      createdAt: createdAt || Date.now(),
+    });
+    await carAd.save();
+    res.status(200).json({ message: "Car ad published successfully", carAd });
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "Failed to publish ad", error: err.message });
+  }
+});
+
+CarAd_router.get("/api/getallcarad", async (req, res) => {
+  try {
+    const cars = await carAd_model.find().sort({ createdAt: -1 });
+
+    res.status(200).json({
+      message: "Fetched successfully",
+      data: cars,
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: "Failed to fetch car ads",
+      error: err.message,
+    });
+  }
+});
+
+CarAd_router.post("/api/getcarbysearch", async (req, res) => {
+  const { name, company, city, enginecapacity } = req.body;
+
+  try {
+    const filter_car = await carAd_model.find({
+      $or: [
+        company ?  {company :{ $regex: company, $options: "i" }} : null,
+        name ? { carname : { $regex : name , $options :"i"}} : null, 
+        enginecapacity ? { enginecapacity : { $regex:enginecapacity ,$options:"i"}} : null,
+        city ? { city : { $regex : city} , $options : "i" } : null,
+      ].filter(Boolean),
+    });
+
+    res.status(200).json({
+      message: "Search successful",
+      data: filter_car,
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: "Search failed",
+      error: err.message,
+    });
+  }
+});
+
+module.exports = CarAd_router;
