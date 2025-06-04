d9fad904b0bf9e4540142268324e2dc99578e3a0
Taha Rauf <rauftaha757@gmail.com>
2025-06-04 05:01:08 +0500

    Update: Modified index.js

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 const express = require("express");
 const authRouter = require("./auth_routes");
-// const dburl =
-//   "mongodb+srv://rauftaha757:ZKj9amdHsA7hpzAT@cluster0.yuh5h6g.mongodb.net/users?retryWrites=true&w=majority&appName=Cluster0";
+const CarAd_router = require("./car_ad_posting_route");
 const dburl = "mongodb://127.0.0.1:27017/auth";
 const mongoose = require("mongoose");
+
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 mongoose
   .connect(dburl)
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
 app.use(authRouter);
-app.use(express.urlencoded({ extended: true }));
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+app.use(CarAd_router);
+
+app.listen(3000,"0.0.0.0", () => {
+  console.log("Server running at http://localhost:3000");
 });
