23f1393737253a79b23bf3ba7844984c28e9eeae
Taha Rauf <rauftaha757@gmail.com>
2025-06-21 10:27:24 +0500

    variable

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
+require('dotenv').config()
 const authRouter = require("./auth_routes");
 const CarAd_router = require("./car_ad_posting_route");
-const dburl = "mongodb://127.0.0.1:27017/auth";
+const dburl = process.env.dburl;
+const port=process.env.port;
 const mongoose = require("mongoose");
 
 const app = express();
@@ -16,6 +18,6 @@ mongoose
 app.use(authRouter);
 app.use(CarAd_router);
 
-app.listen(3000,"0.0.0.0", () => {
+app.listen(port,"0.0.0.0", () => {
   console.log("Server running at http://localhost:3000");
 });
